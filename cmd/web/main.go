e4a17c68ca537321ff2816862093a5b29dfc9dc8
amanycodes <amanycodes@gmail.com>
2024-12-15 18:23:55 +0530

    central error handling and isolated application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -32,19 +32,10 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir(cfg.staticDir))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	srv := &http.Server{
 		Addr:     cfg.addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", cfg.addr)
---
+++ cmd/web/routes.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	var cfg1 config
+
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir(cfg1.staticDir))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	return mux
+}
